424247465f5de8ed255005162f480d7672c43d9f
danbom <52441697+danbom@users.noreply.github.com>
2021-09-01 00:06:17 +0900

    컬러바 setcolor WIP

--- src/components/Colorbar.js
+++ src/components/Colorbar.js
@@ -1,17 +1,68 @@
 import { useContext, useState, useMemo } from "react";
 import React from "react";
+import { ColorContext } from "../components/Letter";
 // import * as S from './styles';
 
 function Colorbar() {
+  const { setColor } = useContext(ColorContext);
+
+  const processColor1 = e => {
+    setColor("#f2dba1");
+  };
+
+  const processColor2 = e => {
+    setColor("#4b4362");
+  };
+
+  const processColor3 = e => {
+    setColor("#6b8f99");
+  };
+
+  const processColor4 = e => {
+    setColor("#b88dcd");
+  };
+
+  const processColor5 = e => {
+    setColor("#db7667");
+  };
+
+  const processColor6 = e => {
+    setColor("#bdbe82");
+  };
+
   return (
     <>
       <div className="colorbar-box">
-        <div className="colorbar-element" id="color1"></div>
-        <div className="colorbar-element" id="color2"></div>
-        <div className="colorbar-element" id="color3"></div>
-        <div className="colorbar-element" id="color4"></div>
-        <div className="colorbar-element" id="color5"></div>
-        <div className="colorbar-element" id="color6"></div>
+        <div
+          className="colorbar-element"
+          id="color1"
+          onClick={processColor1}
+        ></div>
+        <div
+          className="colorbar-element"
+          id="color2"
+          onClick={processColor2}
+        ></div>
+        <div
+          className="colorbar-element"
+          id="color3"
+          onClick={processColor3}
+        ></div>
+        <div
+          className="colorbar-element"
+          id="color4"
+          onClick={processColor4}
+        ></div>
+        <div
+          className="colorbar-element"
+          id="color5"
+          onClick={processColor5}
+        ></div>
+        <div
+          className="colorbar-element"
+          id="color6"
+          onClick={processColor6}
+        ></div>
       </div>
     </>
   );
--- src/components/Letter.js
+++ src/components/Letter.js
@@ -1,27 +1,39 @@
-import React from "react";
+import React, { createContext, useState, useMemo } from "react";
+
 // import * as S from './styles';
 
+export const ColorContext = createContext({
+  setColor: () => {
+    "#f2dba1";
+  },
+});
+
 function Letter() {
+  const [color, setColor] = useState("#f2dba1");
+  const value = useMemo(() => ({ setColor }), [setColor]);
+
   return (
     <>
-      <div className="letter-box">
-        <div className="letter-element" id="to-box">
-          <div className="to-txt">To.</div>
-          <div className="to-underline"></div>
-          <input className="to-contents"></input>
+      <ColorContext.Provider value={value}>
+        <div className="letter-box" style={{ backgroundColor: color }}>
+          <div className="letter-element" id="to-box">
+            <div className="to-txt">To.</div>
+            <div className="to-underline"></div>
+            <input className="to-contents"></input>
+          </div>
+          <div className="letter-element" id="contents-box">
+            <textarea className="contents-contents"></textarea>
+          </div>
+          <div className="letter-element" id="from-box">
+            <div className="from-txt">From.</div>
+            <div className="from-underline"></div>
+            <input
+              className="from-contents"
+              placeholder="*친구가 알아볼 수 있도록 써주세요"
+            ></input>
+          </div>
         </div>
-        <div className="letter-element" id="contents-box">
-          <textarea className="contents-contents"></textarea>
-        </div>
-        <div className="letter-element" id="from-box">
-          <div className="from-txt">From.</div>
-          <div className="from-underline"></div>
-          <input
-            className="from-contents"
-            placeholder="*친구가 알아볼 수 있도록 써주세요"
-          ></input>
-        </div>
-      </div>
+      </ColorContext.Provider>
     </>
   );
 }
