f62b4fb783c771a650a266f0d0f67e184ece9754
mineunyoung <mey990425@gmail.com>
2021-09-02 09:24:51 +0900

    우체통 링크 이름 수정

--- src/components/Txt/LinkName.js
+++ src/components/Txt/LinkName.js
@@ -1,19 +1,15 @@
-import React from 'react';
+import React from "react";
 // import * as S from './styles';
 
 function LinkName() {
+  const Link_name = localStorage.getItem("Link_name");
 
-    return (
-        <>
-            <div className="link-name-small">
-                링크 제목
-            </div>
-            <div className="link-name-big">
-                "000의 우체통"
-            </div>
-
-        </>
-    );
+  return (
+    <>
+      <div className="link-name-small">링크 제목</div>
+      <div className="link-name-big">"{Link_name}의 우체통"</div>
+    </>
+  );
 }
 
 export default LinkName;
