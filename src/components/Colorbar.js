6145845cb886950c1bfa83c2b7064eff0e9e4537
danbom <52441697+danbom@users.noreply.github.com>
2021-09-11 21:14:50 +0900

    컬러바 선택 데코

--- src/components/Colorbar.js
+++ src/components/Colorbar.js
@@ -15,43 +15,79 @@ function Colorbar() {
     // setColor("#DAAE40");
     setColor("#DAAE40");
     localStorage.setItem("letter_color", "#DAAE40");
+    document.getElementById("color1").classList.add("active");
+    document.getElementById("color2").classList.remove("active");
+    document.getElementById("color3").classList.remove("active");
+    document.getElementById("color4").classList.remove("active");
+    document.getElementById("color5").classList.remove("active");
+    document.getElementById("color6").classList.remove("active");
   };
 
   const processColor2 = (e) => {
     // setColor("#4b4362");
     setColor("#85A7CB");
     localStorage.setItem("letter_color", "#85A7CB");
+    document.getElementById("color2").classList.add("active");
+    document.getElementById("color1").classList.remove("active");
+    document.getElementById("color3").classList.remove("active");
+    document.getElementById("color4").classList.remove("active");
+    document.getElementById("color5").classList.remove("active");
+    document.getElementById("color6").classList.remove("active");
   };
 
   const processColor3 = (e) => {
     // setColor("#87AFBA");
     setColor("#87AFBA");
     localStorage.setItem("letter_color", "#87AFBA");
+    document.getElementById("color3").classList.add("active");
+    document.getElementById("color1").classList.remove("active");
+    document.getElementById("color2").classList.remove("active");
+    document.getElementById("color4").classList.remove("active");
+    document.getElementById("color5").classList.remove("active");
+    document.getElementById("color6").classList.remove("active");
   };
 
   const processColor4 = (e) => {
     // setColor("#b88dcd");
     setColor("#b88dcd");
     localStorage.setItem("letter_color", "#b88dcd");
+    document.getElementById("color4").classList.add("active");
+    document.getElementById("color1").classList.remove("active");
+    document.getElementById("color2").classList.remove("active");
+    document.getElementById("color3").classList.remove("active");
+    document.getElementById("color5").classList.remove("active");
+    document.getElementById("color6").classList.remove("active");
   };
 
   const processColor5 = (e) => {
     // setColor("#db7667");
     setColor("#db7667");
     localStorage.setItem("letter_color", "#db7667");
+    document.getElementById("color5").classList.add("active");
+    document.getElementById("color1").classList.remove("active");
+    document.getElementById("color2").classList.remove("active");
+    document.getElementById("color3").classList.remove("active");
+    document.getElementById("color4").classList.remove("active");
+    document.getElementById("color6").classList.remove("active");
   };
 
   const processColor6 = (e) => {
     // setColor("#bdbe82");
     setColor("#bdbe82");
     localStorage.setItem("letter_color", "#bdbe82");
+    document.getElementById("color6").classList.add("active");
+    document.getElementById("color1").classList.remove("active");
+    document.getElementById("color2").classList.remove("active");
+    document.getElementById("color3").classList.remove("active");
+    document.getElementById("color4").classList.remove("active");
+    document.getElementById("color5").classList.remove("active");
   };
 
   return (
     <>
       <div className="colorbar-box">
         <div
-          className="colorbar-element"
+          className="colorbar-element active"
           id="color1"
           onClick={processColor1}
         ></div>
