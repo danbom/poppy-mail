beaaec2036b92144a9108f4621a97e8392bb8461
danbom <52441697+danbom@users.noreply.github.com>
2022-01-09 22:28:46 +0900

    Fix: update delete postbox request

--- src/components/MyPostboxItem1.js
+++ src/components/MyPostboxItem1.js
@@ -80,6 +80,7 @@ function MyPostboxItem1() {
         } else {
           alert("다시 시도해주세요");
         }
+        fetchRequest();
       });
   };
 
@@ -87,88 +88,92 @@ function MyPostboxItem1() {
   const refresh = localStorage.getItem("refresh");
   const [id, setId] = useState("");
 
-  fetch("https://poppymail.shop/mailbox/", {
-    method: "GET",
-    headers: {
-      Authorization: "Bearer " + access,
-      "Content-Type": "application/json",
-      Accept: "application/json",
-    },
-  })
-    .then((res) => res.json())
-    .then((res) => {
-      console.log(res);
-      console.log(id);
-      localStorage.removeItem("sender0");
-      localStorage.removeItem("sender1");
-      localStorage.removeItem("sender2");
-      localStorage.removeItem("sender3");
-      if (res.detail === "Given token not valid for any token type") {
-        fetch("https://poppymail.shop/api/token/refresh/", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            refresh: refresh,
-          }),
-        })
-          .then((res) => res.json())
-          .then((res) => {
-            if (res) {
-              console.log(res);
-              if (res.detail === "Token is invalid or expired") {
-                localStorage.clear();
-              } else {
-                localStorage.setItem("access", res.access);
+  const fetchRequest = () => {
+    fetch("https://poppymail.shop/mailbox/", {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + access,
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+        console.log(id);
+        localStorage.removeItem("sender0");
+        localStorage.removeItem("sender1");
+        localStorage.removeItem("sender2");
+        localStorage.removeItem("sender3");
+        if (res.detail === "Given token not valid for any token type") {
+          fetch("https://poppymail.shop/api/token/refresh/", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              refresh: refresh,
+            }),
+          })
+            .then((res) => res.json())
+            .then((res) => {
+              if (res) {
+                console.log(res);
+                if (res.detail === "Token is invalid or expired") {
+                  localStorage.clear();
+                } else {
+                  localStorage.setItem("access", res.access);
+                }
               }
-            }
-          });
-      }
+            });
+        }
 
-      if (res[0]) {
-        localStorage.setItem("1st_link_title", res[0].link_title);
-        localStorage.setItem("1st_mailbox_link", res[0].mailbox_link);
-        localStorage.setItem("1st_number_letter", res[0].number_of_letter);
-        localStorage.setItem("1st_id", res[0].id);
+        if (res[0]) {
+          localStorage.setItem("1st_link_title", res[0].link_title);
+          localStorage.setItem("1st_mailbox_link", res[0].mailbox_link);
+          localStorage.setItem("1st_number_letter", res[0].number_of_letter);
+          localStorage.setItem("1st_id", res[0].id);
 
-        localStorage.setItem("check_mailbox_today", res.check_mailbox_today);
-        setId(res[0].id);
+          localStorage.setItem("check_mailbox_today", res.check_mailbox_today);
+          setId(res[0].id);
 
-        setItemTitle1(localStorage.getItem("1st_link_title"));
-        setItemLink1(localStorage.getItem("1st_mailbox_link"));
-        setItemLetter1(localStorage.getItem("1st_number_letter"));
-      }
+          setItemTitle1(localStorage.getItem("1st_link_title"));
+          setItemLink1(localStorage.getItem("1st_mailbox_link"));
+          setItemLetter1(localStorage.getItem("1st_number_letter"));
+        }
 
-      if (res[1]) {
-        localStorage.setItem("2nd_link_title", res[1].link_title);
-        localStorage.setItem("2nd_mailbox_link", res[1].mailbox_link);
-        localStorage.setItem("2nd_number_letter", res[1].number_of_letter);
-      }
+        if (res[1]) {
+          localStorage.setItem("2nd_link_title", res[1].link_title);
+          localStorage.setItem("2nd_mailbox_link", res[1].mailbox_link);
+          localStorage.setItem("2nd_number_letter", res[1].number_of_letter);
+        }
 
-      if (res[2]) {
-        localStorage.setItem("3rd_link_title", res[2].link_title);
-        localStorage.setItem("3rd_mailbox_link", res[2].mailbox_link);
-        localStorage.setItem("3rd_number_letter", res[2].number_of_letter);
-      }
+        if (res[2]) {
+          localStorage.setItem("3rd_link_title", res[2].link_title);
+          localStorage.setItem("3rd_mailbox_link", res[2].mailbox_link);
+          localStorage.setItem("3rd_number_letter", res[2].number_of_letter);
+        }
 
-      if (res[3]) {
-        localStorage.setItem("4th_link_title", res[3].link_title);
-        localStorage.setItem("4th_mailbox_link", res[3].mailbox_link);
-        localStorage.setItem("4th_number_letter", res[3].number_of_letter);
-      }
+        if (res[3]) {
+          localStorage.setItem("4th_link_title", res[3].link_title);
+          localStorage.setItem("4th_mailbox_link", res[3].mailbox_link);
+          localStorage.setItem("4th_number_letter", res[3].number_of_letter);
+        }
 
-      if (res[4]) {
-        localStorage.setItem("5th_link_title", res[4].link_title);
-        localStorage.setItem("5th_mailbox_link", res[4].mailbox_link);
-        localStorage.setItem("5th_number_letter", res[4].number_of_letter);
-      }
+        if (res[4]) {
+          localStorage.setItem("5th_link_title", res[4].link_title);
+          localStorage.setItem("5th_mailbox_link", res[4].mailbox_link);
+          localStorage.setItem("5th_number_letter", res[4].number_of_letter);
+        }
 
-      if (res.detail === "User not found") {
-        alert("다시 로그인해주세요!");
-        localStorage.clear();
-      }
-    });
+        if (res.detail === "User not found") {
+          alert("다시 로그인해주세요!");
+          localStorage.clear();
+        }
+      });
+  };
+
+  fetchRequest();
 
   const openSpecificPostboxRequest = () => {
     history.push("/checkarrivedmail/" + id + "/letters/");
