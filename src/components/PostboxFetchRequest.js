e968a46a2591b9a777d5d0c08b03dd42c73b92c2
danbom <52441697+danbom@users.noreply.github.com>
2022-01-13 00:42:44 +0900

    Feat: postboxfetchRequest / refreshRequest  WIP

--- src/components/PostboxFetchRequest.js
+++ src/components/PostboxFetchRequest.js
@@ -22,30 +22,46 @@ export const postBoxFetchRequest = () => {
         localStorage.removeItem("1st_link_title");
         localStorage.removeItem("1st_mailbox_link");
         localStorage.removeItem("1st_number_letter");
+        localStorage.removeItem("1st_open_date");
         localStorage.removeItem("1st_id");
         localStorage.removeItem("2nd_link_title");
         localStorage.removeItem("2nd_mailbox_link");
         localStorage.removeItem("2nd_number_letter");
+        localStorage.removeItem("2nd_open_date");
         localStorage.removeItem("2nd_id");
         localStorage.removeItem("3rd_link_title");
         localStorage.removeItem("3rd_mailbox_link");
         localStorage.removeItem("3rd_number_letter");
+        localStorage.removeItem("3rd_open_date");
         localStorage.removeItem("3rd_id");
         localStorage.removeItem("4th_link_title");
         localStorage.removeItem("4th_mailbox_link");
         localStorage.removeItem("4th_number_letter");
+        localStorage.removeItem("4th_open_date");
         localStorage.removeItem("4th_id");
         localStorage.removeItem("5th_link_title");
         localStorage.removeItem("5th_mailbox_link");
         localStorage.removeItem("5th_number_letter");
+        localStorage.removeItem("5th_open_date");
         localStorage.removeItem("5th_id");
 
         // RefreshRequest(res, refresh);
+        // console.log(res);
+
+        // var step;
+        // for (step = 0; step < res.length; step++) {
+        //   localStorage.setItem("1st_link_title", res[0].link_title);
+        //   localStorage.setItem("1st_mailbox_link", res[0].mailbox_link);
+        //   localStorage.setItem("1st_number_letter", res[0].number_of_letter);
+        //   localStorage.setItem("1st_open_date", res[0].open_date);
+        //   localStorage.setItem("1st_id", res[0].id);
+        // }
 
         if (res[0]) {
           localStorage.setItem("1st_link_title", res[0].link_title);
           localStorage.setItem("1st_mailbox_link", res[0].mailbox_link);
           localStorage.setItem("1st_number_letter", res[0].number_of_letter);
+          localStorage.setItem("1st_open_date", res[0].open_date);
           localStorage.setItem("1st_id", res[0].id);
 
           localStorage.setItem("check_mailbox_today", res.check_mailbox_today);
@@ -55,6 +71,7 @@ export const postBoxFetchRequest = () => {
           localStorage.setItem("2nd_link_title", res[1].link_title);
           localStorage.setItem("2nd_mailbox_link", res[1].mailbox_link);
           localStorage.setItem("2nd_number_letter", res[1].number_of_letter);
+          localStorage.setItem("2nd_open_date", res[1].open_date);
           localStorage.setItem("2nd_id", res[1].id);
         }
 
@@ -62,6 +79,7 @@ export const postBoxFetchRequest = () => {
           localStorage.setItem("3rd_link_title", res[2].link_title);
           localStorage.setItem("3rd_mailbox_link", res[2].mailbox_link);
           localStorage.setItem("3rd_number_letter", res[2].number_of_letter);
+          localStorage.setItem("3rd_open_date", res[2].open_date);
           localStorage.setItem("3rd_id", res[2].id);
         }
 
@@ -69,6 +87,7 @@ export const postBoxFetchRequest = () => {
           localStorage.setItem("4th_link_title", res[3].link_title);
           localStorage.setItem("4th_mailbox_link", res[3].mailbox_link);
           localStorage.setItem("4th_number_letter", res[3].number_of_letter);
+          localStorage.setItem("4th_open_date", res[3].open_date);
           localStorage.setItem("4th_id", res[3].id);
         }
 
@@ -76,6 +95,7 @@ export const postBoxFetchRequest = () => {
           localStorage.setItem("5th_link_title", res[4].link_title);
           localStorage.setItem("5th_mailbox_link", res[4].mailbox_link);
           localStorage.setItem("5th_number_letter", res[4].number_of_letter);
+          localStorage.setItem("5th_open_date", res[4].open_date);
           localStorage.setItem("5th_id", res[4].id);
         }
       }
--- src/components/RefreshRequest.js
+++ src/components/RefreshRequest.js
@@ -1,23 +1,15 @@
 export const RefreshRequest = (res, refresh) => {
-  if (res.detail === "Given token not valid for any token type" && refresh) {
-    fetch("https://poppymail.shop/api/token/refresh/", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        refresh: refresh,
-      }),
-    })
-      .then((res) => res.json())
-      .then((res) => {
-        if (res) {
-          if (res.detail === "Token is invalid or expired") {
-            localStorage.clear();
-          } else {
-            localStorage.setItem("access", res.access);
-          }
-        }
-      });
-  }
+  fetch("https://poppymail.shop/api/token/refresh/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      refresh: refresh,
+    }),
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      localStorage.setItem("access", res.access);
+    });
 };
